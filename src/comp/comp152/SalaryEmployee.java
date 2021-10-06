8503000899fe026002a399cd716cb48b5b524539
anthonydelsignore <adelsignore@student.bridgew.efu>
2021-10-06 16:03:09 -0400

    end of 10/6

---
+++ src/comp/comp152/SalaryEmployee.java
@@ -0,0 +1,14 @@
+package comp.comp152;
+
+public class SalaryEmployee extends Employee{
+
+    private double yearlySalary;
+
+    public SalaryEmployee(double salary){
+        yearlySalary = salary;
+    }
+    @Override
+    public double calculateBiWeeklyPay() {
+        return yearlySalary/26;
+    }
+}
